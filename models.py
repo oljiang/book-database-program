a99818b32f68ec2d42da9899fa56c1d3a040ef0d
Oliver Jiang <oj@MyPC.local>
2022-01-20 15:13:06 -0500

    Initial commit

---
+++ models.py
@@ -0,0 +1,11 @@
+#what the program does
+    #add book to db
+    #view all books
+    #search for a book
+    #book analysis
+    #exit
+
+#create a database
+# books.db
+# create a model
+# title, aurthor, date published, price
